b2c7576218a8081e0ca9552f9cf4be7052df870f
49463282 <49463282@qq.com>
2020-07-23 13:53:07 +0800

    更新

--- Cy_zy/Lw_test.py
+++ Cy_zy/Lw_test.py
@@ -3,38 +3,49 @@ from time import sleep
 
 from selenium.common.exceptions import NoSuchElementException
 
-list = [
-    'Abdominal Manual Therapy Repairs Interstitial Cells of Cajal and Increases Colonic c-Kit Expression When Treating Bowel Dysfunction after Spinal Cord Injury',
-    'Effects of Electroacupuncture on the Daily Rhythmicity of Intestinal Movement and CircadianRhythmicity of Colonic Per2 Expression in Rats with Spinal Cord Injury',
-    'Electroacupuncture at Zusanli (ST36) ameliorates colonic neuronal nitric oxide synthaseupregulation in rats with neurogenic bowel dysfunction following spinal cord injury',
-    'Effectiveness and safety of Chinese massage therapy (Tui Na) on post-stroke spasticity: a prospective multicenter randomized controlled trial',
-    'Surgical versus non-surgical treatment for vertebral compression fracture with osteopenia: a systematic review and meta-analysis',
-    'Tai Chi for improving cardiopulmonary function and quality of life in patients with chronic obstructive pulmonary disease: A systematic review and meta-analysis',
-    ' Clinical Rehabilitation',
-    'Efficiency of muscle strength training on motor function in patients with coronary artery disease: a meta-analysis',
-    'Sling Exercise for Chronic Low Back Pain: A Systematic Review and Meta-Analysis',
-    'Research progress on the central mechanism underlying regulation of visceral biological rhythm by per2 (Review)',
-    '加速康复外科从recovery到rehabilitation']
-for i in list:
-    driver = webdriver.Chrome()
-    driver.get('https://xueshu.baidu.com/')
-    driver.find_element_by_id('kw').send_keys(i)
-    driver.find_element_by_id('su').click()
-    try:
-        name = driver.find_element_by_class_name('author_text').text
-        title = driver.find_element_by_class_name('container_right').text
-        time = driver.find_element_by_class_name('doi_wr').text
-        unit = len(name.split("，"))
-    except:
-        time = 'null'
-        name = 'null'
-        title = 'null'
-        unit = 'null'  # 抛出异常，注释后则不抛出异常
-    print(i)
-    print(time[-12:-5])
-    print(time)
-    print(name)
-    print(title)
-    print(unit)
-    sleep(3)
-    driver.quit()
+
+# driver = webdriver.Chrome()
+# driver.get('http://page5.zzu.edu.cn:8080/system/login.jsp')
+# '''
+# JSESSIONID=24468B3B8DA4195D8E4C1B7A583E74BF;
+# weblogintype=cmslogin;
+# webcorpname=tyx;
+# webskinname=green;
+# wb_login_account_set=tyx;
+# _newscontentshowADV=true;
+# wbcmsshowsysid=mysite'''
+# driver.add_cookie({'name': 'JSESSIONID', 'value': '24468B3B8DA4195D8E4C1B7A583E74BF'})
+# driver.add_cookie({'name': 'weblogintype', 'value': 'cmslogin'})
+# driver.add_cookie({'name': 'webcorpname', 'value': 'tyx'})
+# driver.add_cookie({'name': 'webskinname', 'value': 'green'})
+# driver.add_cookie({'name': 'wb_login_account_set', 'value': 'tyx'})
+# driver.add_cookie({'name': '_newscontentshowADV', 'value': 'true'})
+# driver.add_cookie({'name': 'wbcmsshowsysid', 'value': 'mysite'})
+# sleep(5)
+# driver.quit()
+# i = 0
+# while 2 > 1:
+#     line = 'line_u5_' + str(i)
+#     try:
+#         name = driver.find_element_by_id(line).text
+#         driver.find_element_by_id(line).click()
+#         headline = driver.find_element_by_class_name('border').text
+#         driver.get('http://www5.zzu.edu.cn/tyx/kxyj.htm')
+#         i = int(i) + 1
+#     except:
+#         i = int(i) + 1
+#         line = 'line_u5_' + str(i)
+#         driver.get('http://www5.zzu.edu.cn/tyx/kxyj.htm')
+#         name = driver.find_element_by_id(line).text
+#         driver.find_element_by_id(line).click()
+#         headline = driver.find_element_by_class_name('border').text
+#         driver.get('http://www5.zzu.edu.cn/tyx/kxyj.htm')
+#         if i == 15:
+#             break
+# finally:
+
+# print(time[-12:-5])
+# print(time)
+# print(name)
+# print(headline)
+# print(unit)
--- tools/Redis.py
+++ tools/Redis.py
@@ -6,6 +6,3 @@ class DBRedis:
     @classmethod
     def get_connect(cls):
         return redis.StrictRedis(host='134.175.210.250', port=65379, password='cdMall@321', db=5, decode_responses=True)
-
-
-print(redis.VERSION)
